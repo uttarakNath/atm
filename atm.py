c60fe1d34adc1b001b7fadcc380e871830270374
komrad Nath <77725079+uttarakNath@users.noreply.github.com>
2022-07-23 17:05:25 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__(self, pinno, cardno):
+        self.pinno = pinno
+        self.cardno = pinno
+    
+    def cashWithdrawl(self):
+        print("cash withdrawn")
+
+    def balanceEnq(self):
+        print("balance enquiry")
+
+    def accelerate(self):
+        print("accelerating...")
+
+    def changeGear(self,geartype):
+        print("gear changed")
+
